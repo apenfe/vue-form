b4182c612cb1f64dbb1d6ef57fb03fb11b3d3317
apenfe <adrian1414@hotmail.es>
2024-07-25 11:11:15 +0200

    Css de aplicacion sencilla vue3

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -1,44 +1,52 @@
 <template>
 
-    <section>
+    <section id="form">
 
         <h3>Añadir profesor</h3>
 
-        <div>
-            <label>Nombre:</label>
-            <input type="text" v-model="teacher.teacherName">
-        </div>
+        <div id="data">
 
-        <div>
-            <label>Apellidos:</label>
-            <input type="text" v-model="teacher.teacherSurname">
-        </div>
+            <div>
+                <label>Nombre:</label>
+                <input type="text" v-model="teacher.teacherName">
+            </div>
+    
+            <div>
+                <label>Apellidos:</label>
+                <input type="text" v-model="teacher.teacherSurname">
+            </div>
+    
+            <div>
+                <label>DNI:</label>
+                <input type="text" v-model="teacher.dni">
+            </div>
+    
+            <div>
+                <label>Materias:</label>
+                <input type="text" v-model="subject" >
+                <button @click="handleSubject()">Añadir Materia</button>
+            </div>
+    
+            <div id="listado">
+                <ul>
+                    <li v-for="(elm,index) in teacher.subjects" :key="index">
+                        <h4>{{elm}}</h4>
+                    </li>
+                </ul>
+            </div>
+    
+            <div>
+                <input type="checkbox" v-model="teacher.doc"> Documentacion entregada.
+                <button @click="handleAddTeacher()">Añadir Profesor</button>
+            </div>
 
-        <div>
-            <label>DNI:</label>
-            <input type="text" v-model="teacher.dni">
         </div>
-
-        <div>
-            <label>Materias:</label>
-            <input type="text" v-model="subject" >
-            <button @click="handleSubject()">Añadir Materia</button>
-        </div>
-
-        <div>
-            <ul>
-                <li v-for="(elm,index) in teacher.subjects" :key="index">
-                    <h4>{{elm}}</h4>
-                </li>
-            </ul>
-        </div>
-
-        <input type="checkbox" v-model="teacher.doc"> Documentacion entregada.
-        <button @click="handleAddTeacher()">Añadir Profesor</button>
 
     </section>
 
-    <section>
+    <div class="clearfix"></div>
+
+    <section id="tabla">
 
         <h3>Listado de profesores</h3>
 
@@ -125,5 +133,124 @@
 </script>
 
 <style scoped>
+
+    /* ESTILOS GENERALES */
+
+    *{
+        margin: 0px auto;
+        padding: 0px;
+        text-align: center;
+    }
+
+    .clearfix{
+        float: none;
+        clear: both;
+    }
+
+    /* ESTILOS FORMULARIO */
+
+    #form{
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 18px;
+        height: auto;
+        width: 30%;
+        background-color: #ccc;
+        text-align: center;
+        margin: 10px auto;
+        margin-top: 30px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 20px;
+        box-shadow: 2px 3px 5px #333;
+    }
+
+    #form h3{
+        display: block;
+        font-size: 20px;
+        height: 25px;
+        width: 95%;
+        background-color: #a19c9c;
+        margin: 8px auto;
+        padding: 8px;
+        border: 1px solid gray;
+        border-radius: 10px;
+        box-shadow: 2px 3px 5px rgb(102, 102, 102) inset;
+        color: #333;
+        transition: all 300ms;
+    }
+
+    #form h3:hover{
+        font-size: 22px;
+        border: 1px solid black;
+        border-radius: 10px;
+        box-shadow: 3px 3px 6px black inset;
+        color: #333;
+    }
+
+    #form #data{
+        
+        background-color: #a19c9c;
+        border-radius: 5px;
+        margin: 5px;
+        padding: 10px;
+        box-shadow: 1px 1px 3px #333 inset;
+    }
+
+    #listado ul{
+        list-style: none;
+        width: 100%;
+        margin: 5px;
+        background-color: rgb(131, 97, 97);
+        border-radius: 30px;
+    }
+
+    #listado ul li{
+        width: 45%;
+        margin: 5px auto;
+        background-color: rgb(95, 23, 23);
+        text-decoration: none;
+        border-radius: 100px;
+
+    }
+
+    /* ESTILOS TABLA */
+
+    #tabla{
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 18px;
+        height: auto;
+        width: 50%;
+        background-color: #ccc;
+        text-align: center;
+        margin: 10px auto;
+        margin-top: 30px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 20px;
+        box-shadow: 2px 3px 5px #333;
+    }
+
+    #tabla h3{
+        display: block;
+        font-size: 20px;
+        height: 25px;
+        width: 95%;
+        background-color: #a19c9c;
+        margin: 8px auto;
+        padding: 8px;
+        border: 1px solid gray;
+        border-radius: 10px;
+        box-shadow: 2px 3px 5px rgb(102, 102, 102) inset;
+        color: #333;
+        transition: all 300ms;
+    }
+
+    #tabla h3:hover{
+        font-size: 22px;
+        border: 1px solid black;
+        border-radius: 10px;
+        box-shadow: 3px 3px 6px black inset;
+        color: #333;
+    }
 
 </style>
